841bd12f9b3ad9f367675ee472e8e185f7821e1c
Jingxun Yin <jingxunyin1@gmail.com>
2022-06-23 18:14:05 -0400

    uauth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
+
+
+
 
 module.exports = router;
