4572dc1f82d153d51dcafe9a0923bfa96336cb2d
Jingxun Yin <jingxunyin1@gmail.com>
2022-06-23 18:48:56 -0400

    ph

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,9 +3,23 @@ const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
 
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
       const { credential, password } = req.body;
 
@@ -33,5 +47,17 @@ router.delete(
     return res.json({ message: 'success' });
 }
 );
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
 
 module.exports = router;
