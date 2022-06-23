b07f769e4c3be9521dcdc331a62d08f6e8b222a2
Jingxun Yin <jingxunyin1@gmail.com>
2022-06-23 16:17:28 -0400

    ph routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
+});
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
 });
 
 module.exports = router;
