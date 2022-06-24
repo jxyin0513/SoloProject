34e30efef786d47c7240e5fa0cbeb373bdadcd3f
Jingxun Yin <jingxunyin1@gmail.com>
2022-06-24 10:25:38 -0400

    thunk reducer

---
+++ frontend/src/store/session.js
@@ -0,0 +1,53 @@
+import { csrfFetch } from "./csrf";
+
+const ADD_SESSION = "/add/session";
+const REMOVE_SESSION = "/remove/session"
+
+export const addUser = (user)=>{
+    return {
+        type: ADD_SESSION,
+        user
+    }
+}
+
+export const removeUser = (userId)=>{
+    return{
+        type: REMOVE_SESSION,
+        userId
+    }
+}
+
+export const addUserThunk = (user)=> async (dispatch)=>{
+    const {credential, password} = user
+    const response = await csrfFetch('/api/session',{
+        method: "POST",
+        body: JSON.stringify({credential, password})
+    })
+    if(response.ok){
+        const data = await response.json();
+        dispatch(addUser(data.user));
+        return user;
+    }else{
+        return false;
+    }
+}
+
+const initialState = { user: null }
+
+const sessionReducer = (state=initialState, action)=>{
+    let newState = {...state}
+    switch(action.type){
+        case ADD_SESSION:
+            console.log("here")
+            newState.user= action.user;
+            return newState;
+        case REMOVE_SESSION:
+            newState.user = null;
+            return newState;
+        default:
+            return state;
+
+    }
+}
+
+export default sessionReducer;
