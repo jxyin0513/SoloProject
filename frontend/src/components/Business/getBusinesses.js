a39bb00bddb17d7d24a10e3059b008d2b7646675
Jingxun Yin <jingxunyin1@gmail.com>
2022-06-28 10:39:08 -0400

    fix

--- frontend/src/components/Business/getBusinesses.js
+++ frontend/src/components/Business/getBusinesses.js
@@ -1,18 +1,18 @@
-import React, {useState} from "react";
-import {useDispatch, useSelector} from 'react-redux';
+// import React, {useState} from "react";
+// import {useDispatch, useSelector} from 'react-redux';
 
-function AllBusinesses(){
-    const allBusinesses = useSelector(state=>state.allBusinesses);
+// function AllBusinesses(){
+//     const allBusinesses = useSelector(state=>state.allBusinesses);
 
-    return (
-        <>
-        {allBusinesses&&(
-            <li>
-                {Object.values(allBusinesses).map(business=>
-                    <ul key={business.id}>{business.name}</ul>
-                )}
-            </li>)
-            }
-        </>
-    )
-}
+//     return (
+//         <>
+//         {allBusinesses&&(
+//             <li>
+//                 {Object.values(allBusinesses).map(business=>
+//                     <ul key={business.id}>{business.name}</ul>
+//                 )}
+//             </li>)
+//             }
+//         </>
+//     )
+// }
